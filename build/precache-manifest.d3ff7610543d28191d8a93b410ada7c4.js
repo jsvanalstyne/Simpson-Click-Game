3738c55193abe722bab73cb3ea61866e979010c3
Jerrica VanAlstyne <jsnow07@yahoo.com>
2020-02-02 22:30:58 -0500

    deployed

---
+++ build/precache-manifest.d3ff7610543d28191d8a93b410ada7c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef1774f03941bc3e9171f543b728679",
+    "url": "/Simpson-Click-Game/index.html"
+  },
+  {
+    "revision": "db8b9dfaab5b9181a9a6",
+    "url": "/Simpson-Click-Game/static/css/main.18c24124.chunk.css"
+  },
+  {
+    "revision": "450a196835b0ab458a50",
+    "url": "/Simpson-Click-Game/static/js/2.6a03fab3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Simpson-Click-Game/static/js/2.6a03fab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8b9dfaab5b9181a9a6",
+    "url": "/Simpson-Click-Game/static/js/main.20a838b5.chunk.js"
+  },
+  {
+    "revision": "9e50215b6c453f878fec",
+    "url": "/Simpson-Click-Game/static/js/runtime-main.c7622434.js"
+  }
+]);
