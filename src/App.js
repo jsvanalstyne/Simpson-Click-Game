057853b56f6bf4e0bf887cdc67eff549be00cf5f
Jerrica VanAlstyne <jsnow07@yahoo.com>
2020-02-02 21:33:17 -0500

    changed some stylings, added game logic and sorting functions

--- src/App.js
+++ src/App.js
@@ -7,17 +7,48 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer"
 import character from "./simpson.json";
 import Container from "./components/Container"
+import Wrapper from "./components/Wrapper"
 class App extends Component {
   state= {
     score: 0,
     topscore: 0,
     character: character,
-    clicked: []
+    alreadyClicked: []
     
+  }
+
+  handleClick = id => {
+  
+    this.setState({
+      // put the card id into the already clicked array
+      alreadyClicked: this.state.alreadyClicked.concat(id)
+    })
+    // Check to see if the alreadyClicked array includes the current card id, if it does then set the score to zero and empty already clicked array
+    if(this.state.alreadyClicked.includes(id)) {
+      this.setState({
+        score: 0,
+        alreadyClicked: []
+      })
+    }
+    else {
+      // if it does not include the card, increase the score by 1. 
+      this.setState({
+        score: this.state.score + 1,
+        // randomly sorts the array
+        character: character.sort(function(a, b){return 0.5 - Math.random()})
+      })
+      // check to see if the current score is greater than the top score, if it is then replaced the topscore with the current score. 
+      if (this.state.score >= this.state.topscore) {
+        this.setState({
+          topscore: this.state.topscore +1
+        })
+      }
+    }
   }
   render() {
     
     return (
+      // <Wrapper>
       <div>
         <Nav game="Clicky Game" start="Click on an image to begin" score={this.state.score} topscore={this.state.topscore}></Nav>
      
@@ -25,15 +56,16 @@ class App extends Component {
         <h1>Clicky Game!</h1>
         <h2>Click on an image to earn points, but don't click on any more than once!</h2>
       </Jumbo>
-      <Container>
+      <div className="container">
         {this.state.character.map(characters=>(
-          <FunCard id={characters.id} url={characters.url} clicked={characters.clicked}
+          <FunCard id={characters.id} url={characters.url} clicked={characters.clicked} handleClick={this.handleClick}
           ></FunCard>
           ))}
          
-      </Container>
+      </div>
       <Footer></Footer>
       </div>
+      
     )
   }
 }
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -4,8 +4,8 @@ import "./style.css"
 function FunCard(props) {
     return (
         <div>
-            <div className="card col-2 ml-3 mr-3 mb-3">
-            <img className="images" onClick={() => props.handleClick(props.id)} clicked={props.clicked} key={props.id} id={props.id} src={props.url} className="card-img-top" alt="..." />
+            <div className="card col-3 mt-3 ml-3 mr-3 mb-3 shadow p-3 mb-5 bg-white rounded">
+            <img className="images" onClick={() => props.handleClick(props.id)} clicked={props.clicked} key={props.id} id={props.id} src={props.url} alt="..." />
         </div>
             </div>
             )
